cad1e77c0b8081a9cedae13bfa2f4b3b89249ec8
mishio-n <doncool340@gmail.com>
2021-06-16 21:33:16 +0900

    feat: リストに画像を表示

--- components/articles/index.tsx
+++ components/articles/index.tsx
@@ -1,5 +1,7 @@
+import classNames from 'classnames'
 import Link from 'next/link'
 import { Blog } from '~/schema'
+import styles from './articles.module.scss'
 
 type Props = {
   blogs: Blog[]
@@ -12,7 +14,20 @@ export const Articles: React.FC<Props> = ({ blogs }) => {
         {blogs.map((blog) => (
           <li key={blog.id}>
             <Link href={`/blog/${blog.id}`}>
-              <a>{blog.title}</a>
+              <a>
+                <picture>
+                  <source
+                    type="image/webp"
+                    data-srcset={`${blog.ogimage.url}?w=670&fm=webp`}
+                  />
+                  <img
+                    data-src={`${blog.ogimage.url}?w=670`}
+                    alt="img"
+                    className={classNames('lazyload', styles.opimage)}
+                  />
+                </picture>
+                {blog.title}
+              </a>
             </Link>
           </li>
         ))}
