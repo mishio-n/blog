560a31a4a18e56ea1727aed8cefe91e1b88fb71a
mishio-n <doncool340@gmail.com>
2021-06-27 21:59:02 +0900

    style: asideの幅を調整

--- components/Aside/index.tsx
+++ components/Aside/index.tsx
@@ -8,9 +8,7 @@ type Prop = {
 export const Aside: React.FC<Prop> = ({ categories }) => {
   return (
     <aside className="w-80 md:mt16 sm:w-auto sm:mt-16">
-      <p>著者紹介</p>
       <CategoryList categories={categories} />
-      <p>人気の記事</p>
     </aside>
   )
 }
