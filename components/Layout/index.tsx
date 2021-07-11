bc6b34233f0fab96a0e0de96be7494a6d6489bd8
mishio-n <doncool340@gmail.com>
2021-07-11 14:50:48 +0900

    feat: meta情報付与

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
+import { Aside } from '~/components/Aside'
 import { Footer } from '~/components/Footer'
 import { Header } from '~/components/Header'
+import {
+  DESCRIPTION,
+  OG_DESCRIPTION,
+  OG_IMAGE,
+  OG_TYPE,
+  SITE_URL
+} from '~/libs/meta'
 import { Category } from '~/schema'
-import { Aside } from '../Aside'
 
 type Props = {
   categories: Category[]
@@ -10,6 +18,17 @@ type Props = {
 export const Layout: React.FC<Props> = ({ children, categories }) => {
   return (
     <>
+      <Head>
+        <meta key={DESCRIPTION} name={DESCRIPTION} content={''} />
+        <meta key={OG_DESCRIPTION} property={OG_DESCRIPTION} content={''} />
+        <meta key={OG_TYPE} property={OG_TYPE} content="website" />
+        <meta
+          key={OG_IMAGE}
+          property={OG_IMAGE}
+          content={`${SITE_URL}/og.png`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <Header />
       <div className="mx-auto mt-5 mb-0 >1024:flex >1024:justify-between >1024:w-lg md:w-md sm:mx-0 sm:py-0 sm:px-5">
         <div className="lg:w-md">{children}</div>
